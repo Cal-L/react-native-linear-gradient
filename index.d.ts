5f5417ab7cea1967f6e764536e6cf316b286ec61
Ian J Sikes <ianjsikes@gmail.com>
2018-05-22 18:27:11 -0700

    Rename start and end points to fix issues with new RN version

--- index.d.ts
+++ index.d.ts
@@ -2,13 +2,15 @@ import * as React from "react";
 import * as ReactNative from "react-native";
 
 declare module "react-native-linear-gradient" {
+  interface LinearGradientProps extends ReactNative.ViewProperties {
+    colors: string[];
+    startPoint?: { x: number; y: number };
+    endPoint?: { x: number; y: number };
+    locations?: number[];
+  }
 
-    interface LinearGradientProps extends ReactNative.ViewProperties {
-        colors: string[],
-        start?: { x: number, y: number },
-        end?: { x: number, y: number },
-        locations?: number[]
-    }
-
-    export default class LinearGradient extends React.Component<LinearGradientProps, any> { }
+  export default class LinearGradient extends React.Component<
+    LinearGradientProps,
+    any
+  > {}
 }
